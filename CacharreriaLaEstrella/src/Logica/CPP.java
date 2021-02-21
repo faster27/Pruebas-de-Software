40299a3822f653f8a03766dae071eae0bcd581ee
dcorr <dcorrealenis@gmail.com>
2021-02-21 16:08:38 -0500

    y los fiados ya funcionan

--- CacharreriaLaEstrella/src/Logica/CPP.java
+++ CacharreriaLaEstrella/src/Logica/CPP.java
@@ -15,7 +15,7 @@ public class CPP {
     
     
     
-    int pruebaJenkins;
+    int pruebaJenkins
     
     public static double ActualizarCpp(int CantidadEnStock, double CppActual, int UnidadesNuevas, double CostoCompraUnidad){
     
