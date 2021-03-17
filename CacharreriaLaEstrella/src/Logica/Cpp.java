3e20d85998909ab7f2e5e9d4eca529ac685b0b2e
dcorr <dcorrealenis@gmail.com>
2021-03-16 19:50:15 -0500

    La clase Cpp se le hizo el checkstyle

---
+++ CacharreriaLaEstrella/src/Logica/Cpp.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Logica;
+
+
+
+/**.
+ *
+ * @author dcorr
+ */
+
+public class Cpp {
+  /**
+   * 
+   * @param CantidadEnStock .
+   * @param CppActual .
+   * @param UnidadesNuevas .
+   * @param CostoCompraUnidad .
+   * @return .
+   */
+    
+  public static double ActualizarCpp(int CantidadEnStock, double CppActual, 
+          int UnidadesNuevas, double CostoCompraUnidad){
+    
+        
+    System.out.println(CantidadEnStock);
+    System.out.println(CppActual);
+    System.out.println(UnidadesNuevas);
+    System.out.println(CostoCompraUnidad);
+        
+
+    //calcular el cpp
+    double cpp_new;
+    
+    cpp_new = (CantidadEnStock * CppActual + UnidadesNuevas * CostoCompraUnidad)
+            / (CantidadEnStock + UnidadesNuevas);
+        
+    //System.out.println(cpp_new);
+    return cpp_new;   
+  }
+    
+    
+    
+}
