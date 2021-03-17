ef515a0c7293d9f934cefdc73bbc2ad104ff2aca
dcorr <dcorrealenis@gmail.com>
2021-03-17 12:15:35 -0500

    la clase cpp se le agrego descripcion en el javadoc

--- CacharreriaLaEstrella/src/Logica/Cpp.java
+++ CacharreriaLaEstrella/src/Logica/Cpp.java
@@ -16,10 +16,10 @@ package Logica;
 public class Cpp {
   /**
    * 
-   * @param CantidadEnStock .
-   * @param CppActual .
-   * @param UnidadesNuevas .
-   * @param CostoCompraUnidad .
+   * @param CantidadEnStock Cantidad de un producto que se tiene en bodega
+   * @param CppActual Nos dice cual es el valor cpp actual del producto
+   * @param UnidadesNuevas cuantas unidades se compraron en el nuevo ingreso
+   * @param CostoCompraUnidad valor unitario del producto
    * @return .
    */
     
