76765059c3d5c00e1d1ca84412586bfc4e6a54fc
dcorr <dcorrealenis@gmail.com>
2021-03-19 15:46:48 -0500

    del paquete vista ya esta listo GuiFiado

--- CacharreriaLaEstrella/src/vista/GUIPrincipal.java
+++ CacharreriaLaEstrella/src/vista/GUIPrincipal.java
@@ -155,7 +155,7 @@ public class GUIPrincipal extends javax.swing.JFrame {
     private void jButton1InformesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1InformesActionPerformed
         // TODO add your handling code here:
         
-        GUIInformes VentanaInformes = new GUIInformes();
+        GuiInformes VentanaInformes = new GuiInformes();
         VentanaInformes.setVisible(true);
         
        //SELECT productos.categoria, SUM(venta.totalventa) as TotalCategoria
